e42c15ed8f0463432f7f94a511ab7d899c0f8eb5
Péter Ott <ottpeter@tuta.io>
2021-05-06 11:27:09 +0200

    After Cleanup

--- bin/index.js
+++ bin/index.js
@@ -4,27 +4,20 @@ const chalk = require("chalk");
 const boxen = require("boxen");
 const fs =require("fs");
 const levenshtein = require('js-levenshtein');
-
-var bip39 = require ('bip39');                        // Convert mnemonic to seed tool
+const bip39 = require ('bip39');                        // Convert mnemonic to seed tool
 const wordlist = bip39.wordlists.english;
+const HDKey = require('hdkey');                         // Generate from HD path tool
+const ethUtils = require('ethereumjs-util');            // Convert to address tool
 
-var HDKey = require('hdkey');                         // Generate from HD path tool
-var ethUtils = require('ethereumjs-util');            // Convert to address tool
+const FAV = [2,4,8,16,32,64,128,256,512,1024,9,11,20,29,38,110,0];
 
-// This is just message styling...
-const greeting = chalk.white.bold("Start!");
-const msgBox = boxen( greeting, { padding: 1, margin: 1, borderStyle: "round", borderColor: "green", backgroundColor: "#555555"} );
-console.log(msgBox);
-const success = chalk.green.bold("SUCCESS!");
-const sucBox = boxen( success, { padding: 1, margin: 1, borderStyle: "round", borderColor: "green", backgroundColor: "#220022"} );
-
+/** Settings */
 // MAX values
 const MAX_PURPOSE = 44;
 const MAX_COINTYPE = 60;
-const MAX_ACCOUNT = 0;
-const MAX_CHANGE = 0;
+const MAX_ACCOUNT = 100;
+const MAX_CHANGE = 100;
 const MAX_INDEX = 100;
-
 // START values
 const START_PURPOSE = 44;
 const START_COINTYPE = 60;
@@ -32,87 +25,41 @@ const START_ACCOUNT = 0;
 const START_CHANGE = 0;
 const START_INDEX = 0;
 
-const FAV = [2,4,8,16,32,64,128,256,512,1024,9,11,20,29,38,110,0];
-
-
-// The credentials
-let origArray = ["beach", "ceiling", "square", "fiscal", "success", "ghost", "lady", "gossip", "betray", "ostrich", "canal", "average", "sting", "blind", "exchange", "assault", "since", "they", "wet", "hen", "spin", "age", "mechanic", "coil"];
-// Most likely this is not a mnemonic problem
-let levMatrix = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []];	// Array of arrays, each array representing specific position of mnemoic. Those words will be listed, which Levenshtein distance is 1 or 2
-let generatedMnemonics = [];
-let mnemonic = "beach ceiling square fiscal success ghost lady gossip betray ostrich canal average sting blind exchange assault since they wet hen spin age mechanic coil";
-let password = "Oxy63Lufiyogasii613";
-let lookingForAddr = "0x05ff2FA8D64F55627b9B0F8911Ff8d80c481C973";
-let seed = null;
-let hdkey = null;
+/** Settings */
+// Wallet data
+let mnemonic = "fresh outdoor blue asthma trigger digital come language alarm style purpose giant hello ghost jar person zero select canvas mercy reveal prison boss finish";
+let password = "BadPassword";
+let lookingForAddr = "0x11d7F8337C03459101D38DE3B4ABaBFE692Cf711";
 let childkey = null;
 let address = null;
-//let path = "m/44'/60'/0'/0/0";
+let origArray = [];
+let levMatrix = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []];	// Array of arrays, each array representing specific position of mnemoic. Those words will be listed, which Levenshtein distance is 1 or 2
+let generatedMnemonics = [];
 let hdkeyArray = [];
 let seedArray = [];
 
-fillLevenshteinMatrix();
-generateMnemonics();
-
-function  fillLevenshteinMatrix() {
-  for (let i = 0; i < 24; i++) {					// Go through mnemonic array
-    for (let k = 0; k < wordlist.length; k++) {				// Go through wordlist array
-      let distance = levenshtein(origArray[i], wordlist[k]);
-      if (distance == 1 || distance == 1) {
-        console.log("distance: ", distance);
-        console.log("mnemonic word: ", origArray[i]);
-        console.log("similar: ", wordlist[k]);
-        levMatrix[i].push(wordlist[k]);
-      }
-    }
-  }
-  console.log("levMatrix: ", levMatrix);
-}
-
-
-function generateMnemonics() {
-  for (let i = 0; i < 24; i++) {
-    for (let k = 0; k < levMatrix[i].length; k++) {
-      generatedMnemonics.push(generateMnemonic(levMatrix[i][k], i));
-    }
-  }
-  console.log("generated mnemonics: ", generatedMnemonics);
-}
-
-function generateMnemonic(word, position) {
-  let theMnemonic = "";
-  for (let i = 0; i < 24; i++) {
-    if (i == position) {			// Swap
-      theMnemonic = theMnemonic + " " + word;
-    } else {
-      theMnemonic = theMnemonic + " " + origArray[i];
-    }
-  }
-  theMnemonic = theMnemonic.slice(1);
-  return theMnemonic;
-}
-
+// This is just message styling...
+const greeting = chalk.white.bold("Start!"); const startMessage = boxen( greeting, { padding: 1, margin: 1, borderStyle: "round", borderColor: "green", backgroundColor: "#555555"} );
+const success = chalk.green.bold("SUCCESS!"); const successMessage = boxen( success, { padding: 1, margin: 1, borderStyle: "round", borderColor: "green", backgroundColor: "#220022"} );
 
 /** Init */
-seed = bip39.mnemonicToSeedSync(mnemonic, password);                // Convert mnemoic to seed
-seed2 = bip39.mnemonicToSeedSync(mnemonic, "");                // Convert mnemoic to seed
-//console.log("bip39: ", seed);
-hdkey = HDKey.fromMasterSeed(Buffer.from(seed, 'hex'));             // Generate master key from seed
-hdkey2 = HDKey.fromMasterSeed(Buffer.from(seed, 'hex'));             // Generate master key from seed
-
-/** For generated mnemonics */
-for (let i = 0; i < generatedMnemonics.length; i++) {
-  seedArray[i] = bip39.mnemonicToSeedSync(generatedMnemonics[i], password);
-  hdkeyArray[i] = HDKey.fromMasterSeed(Buffer.from(seedArray[i], 'hex'));
-}
-/** For generated mnemonics without pass*/
-for (let i = generatedMnemonics.length; i < generatedMnemonics.length*2; i++) {
-  seedArray[i] = bip39.mnemonicToSeedSync(generatedMnemonics[i], "");
-  hdkeyArray[i] = HDKey.fromMasterSeed(Buffer.from(seedArray[i], 'hex'));
-}
+let seed = bip39.mnemonicToSeedSync(mnemonic, password);                           // Convert mnemoic to seed
+let hdkey = HDKey.fromMasterSeed(Buffer.from(seed, 'hex'));                        // Generate master key from seed
+let seedWithoutPass = bip39.mnemonicToSeedSync(mnemonic, "");                      // Convert mnemoic to seed (no password)
+let hdkeyWithoutPass = HDKey.fromMasterSeed(Buffer.from(seedWithoutPass, 'hex'));  // Generate master key from seed (no password)
+//seedAndKeyForGenerated();                                                        // Only if we use Levanshtein
+//console.log("seed (with pass): ", seed);
+//console.log("seed (without pass): ", seedWithoutPass);
 
 
-forAll();
+/** RUN */
+//fillLevenshteinMatrix();
+//generateMnemonics();
+console.log(startMessage);
+forAll();         // or forFav()
+/** END */
+
+
 /** 5 for cycles all together */
 function forAll() {
   for (let purpose = START_PURPOSE; purpose <= MAX_PURPOSE; purpose++ ) {
@@ -160,51 +107,29 @@ function runVariations(purpose, coinType, account, change, index) {
 
 
 function deriveAddress(path) {
+  // With password
   childkey = hdkey.derive(path);
   address = ethUtils.publicToAddress(childkey.publicKey, true);
   let currentAddr = ethUtils.bufferToHex(address);
-
-  console.log("path: ", path);
-  console.log("Generated address: ", currentAddr);
+  console.log("path (with password)   : ", path);
+  console.log("Generated address      : ", currentAddr);
   
   if (currentAddr.includes(lookingForAddr) || ethUtils.toChecksumAddress(currentAddr).includes(lookingForAddr)) {
-    fs.writeFileSync( "output.txt", "Found! " + path)
-    console.log(sucBox);
-    console.log("Path: ", path);
-    process.exit();
+    done(path);
   } 
 
-// Passwordless
-  childkey = hdkey2.derive(path);
+  // Without password
+  childkey = hdkeyWithoutPass.derive(path);
   address = ethUtils.publicToAddress(childkey.publicKey, true);
   currentAddr = ethUtils.bufferToHex(address);
-
-  console.log("path: ", path);
-  console.log("Generated address: ", currentAddr);
+  console.log("path (without password): ", path);
+  console.log("Generated address      : ", currentAddr);
   
   if (ethUtils.toChecksumAddress(currentAddr).includes(ethUtils.toChecksumAddress(lookingForAddr))) {
-    fs.writeFileSync( "output.txt", "Found! " + path)
-    console.log(sucBox);
-    console.log("Path: ", path);
-    process.exit();
+    done(path);
   } 
 
-  for (let i = 0; i < hdkeyArray.length; i++) {
-    childkey = hdkeyArray[i].derive(path);
-    address = ethUtils.publicToAddress(childkey.publicKey, true);
-    currentAddr = ethUtils.bufferToHex(address);
-
-    console.log("path: ", path);
-    console.log("Generated address: ", currentAddr);
-  
-    if (ethUtils.toChecksumAddress(currentAddr).includes(ethUtils.toChecksumAddress(lookingForAddr))) {
-      fs.writeFileSync( "output.txt", "Found! " + path)
-      console.log(sucBox);
-      console.log("Path: ", path);
-      process.exit();
-    }
-  }
-
+  //deriveForGenerated();                                                     // for generated mnemonics only
 }
 
 function all(purpose, coinType, account, change, index) {
@@ -280,6 +205,7 @@ function withoutAccount(purpose, coinType, change, index) {
   deriveAddress(path);
 }
 
+// Shorter path (without 'account' and 'change')
 function withoutAccountAndChange(purpose, coinType, index) {
   path = "m/" + purpose + "/" + coinType + "/"  + index;
   deriveAddress(path);
@@ -289,4 +215,78 @@ function withoutAccountAndChange(purpose, coinType, index) {
   deriveAddress(path);
   path = "m/" + purpose + "'/" + coinType + "'/" + index;
   deriveAddress(path);
+}
+
+
+
+function  fillLevenshteinMatrix() {
+  for (let i = 0; i < 24; i++) {					// Go through mnemonic array
+    for (let k = 0; k < wordlist.length; k++) {				// Go through wordlist array
+      let distance = levenshtein(origArray[i], wordlist[k]);
+      if (distance == 1 || distance == 1) {
+        console.log("distance: ", distance);
+        console.log("mnemonic word: ", origArray[i]);
+        console.log("similar: ", wordlist[k]);
+        levMatrix[i].push(wordlist[k]);
+      }
+    }
+  }
+  console.log("levMatrix: ", levMatrix);
+}
+
+
+function generateMnemonics() {
+  for (let i = 0; i < 24; i++) {
+    for (let k = 0; k < levMatrix[i].length; k++) {
+      generatedMnemonics.push(generateMnemonic(levMatrix[i][k], i));
+    }
+  }
+  console.log("generated mnemonics: ", generatedMnemonics);
+}
+
+function generateMnemonic(word, position) {
+  let theMnemonic = "";
+  for (let i = 0; i < 24; i++) {
+    if (i == position) {			// Swap
+      theMnemonic = theMnemonic + " " + word;
+    } else {
+      theMnemonic = theMnemonic + " " + origArray[i];
+    }
+  }
+  theMnemonic = theMnemonic.slice(1);
+  return theMnemonic;
+}
+
+function seedAndKeyForGenerated() {
+  /** For generated mnemonics */
+  for (let i = 0; i < generatedMnemonics.length; i++) {
+    seedArray[i] = bip39.mnemonicToSeedSync(generatedMnemonics[i], password);
+    hdkeyArray[i] = HDKey.fromMasterSeed(Buffer.from(seedArray[i], 'hex'));
+  }
+  /** For generated mnemonics without pass*/
+  for (let i = generatedMnemonics.length; i < generatedMnemonics.length*2; i++) {
+    seedArray[i] = bip39.mnemonicToSeedSync(generatedMnemonics[i], "");
+    hdkeyArray[i] = HDKey.fromMasterSeed(Buffer.from(seedArray[i], 'hex'));
+  }
+}
+
+function deriveForGenerated() {
+  for (let i = 0; i < hdkeyArray.length; i++) {
+    childkey = hdkeyArray[i].derive(path);
+    address = ethUtils.publicToAddress(childkey.publicKey, true);
+    currentAddr = ethUtils.bufferToHex(address);
+    console.log("path: ", path);
+    console.log("Generated address: ", currentAddr);
+  
+    if (ethUtils.toChecksumAddress(currentAddr).includes(ethUtils.toChecksumAddress(lookingForAddr))) {
+      done(path);
+    }
+  }
+}
+
+function done(path) {
+  fs.writeFileSync( "output.txt", "Found! " + path);
+  console.log(successMessage);
+  console.log("Path: ", path);
+  process.exit();
 }
